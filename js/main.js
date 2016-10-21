d1907f269718367e56a4488ec3e3540d91b9d61b
Veniamin Zolotukhin <venyaz@gmail.com>
2016-10-21 16:29:46 +0300

    Load gallery item via AJAX

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(function() {
 	console.log('init');
 	var autoloadTimer;
+	var gallery = $('.gallery');
+	galleryLoadMore();
 
 	$('#gallery-load-more').click(function() {
 		galleryLoadMore();
@@ -23,5 +25,24 @@ $(function() {
 
 	function galleryLoadMore() {
 		console.log('Load more...');
+		var xhr = $.ajax({
+			dataType: "json",
+  			url: "data/items.json",
+  			// data: data,
+  			// success: success
+		})
+		.done(function(data) {
+			$.each(data, function(i, item) {
+				gallery.append('<li class="gallery-item">'
+					+ '<img class="gallery-item-photo" src="' + item.image + '">'
+					+ '<span class="gallery-item-rating ui-rating'+ (item.rating ? ' ui-rating-'+item.rating : '') +'"></span>' 
+					+ '<div class="gallery-item-title">' + item.title + '</div>' 
+					+ '<div class="gallery-item-description">' + item.paragraph + '</div>' 
+					+ '</li>');
+			})
+		})
+		.fail(function(data) {
+			console.log('AJAX failed');
+		})
 	}
 });
